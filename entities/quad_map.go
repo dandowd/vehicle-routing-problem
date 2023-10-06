93a9b5dd0d0d5eff0814f1a0886def84c5b0db51
Dan Dowd <danieldowd89@gmail.com>
2023-10-06 13:06:21 -0600

    reworked quad map

--- entities/quad_map.go
+++ entities/quad_map.go
@@ -1,99 +1,65 @@
 package entities
 
-import "math"
-
 const MAX_DRIVE_TIME = 720
 const MAX_DISTANCE = MAX_DRIVE_TIME / 2
 
 type QuadMap struct {
-  parent *QuadMap
-  subMaps [4]*QuadMap
-  loads []*Load
-  minX, maxX, minY, maxY float64
+	parent                 *QuadMap
+	zoomLevel              int
+	subMaps                [4]*QuadMap
+	loads                  []*Load
+	originX, originY float64
 }
 
-func (m* QuadMap) HasPoint(p Point) bool {
-  return p.X >= m.minX && p.X <= m.maxX && p.Y >= m.minY && p.Y <= m.maxY
+func (m *QuadMap) HasPoint(p Point) bool {
+	var maxDistance = float64(MAX_DISTANCE / m.zoomLevel)
+	return p.X >= m.originX-maxDistance && p.X <= m.originX+maxDistance && p.Y >= m.originY-maxDistance && p.Y <= m.originY+maxDistance
 }
 
-func (m* QuadMap) AddLoad(l* Load) {
-  m.loads = append(m.loads, l)
+func (m *QuadMap) AddLoad(l *Load) {
+	m.loads = append(m.loads, l)
 }
 
 // Generates children and adds loads based off pickup location
-// This map has blind spots based off the smallest non-zero float64
 // TODO: Add dropoff location for better heuristics
-func (m* QuadMap) generateChildren() *QuadMap {
-  zeroMap := &QuadMap{m, [4]*QuadMap{}, nil, math.SmallestNonzeroFloat64, MAX_DISTANCE, 0, MAX_DISTANCE}
-  oneMap := &QuadMap{m, [4]*QuadMap{}, nil, -MAX_DISTANCE, 0, 0, MAX_DISTANCE}
-  twoMap := &QuadMap{m, [4]*QuadMap{}, nil, -MAX_DISTANCE, 0, -MAX_DISTANCE, -math.SmallestNonzeroFloat64}
-  threeMap := &QuadMap{m, [4]*QuadMap{}, nil, math.SmallestNonzeroFloat64, MAX_DISTANCE, -MAX_DISTANCE, -math.SmallestNonzeroFloat64}
+func (m *QuadMap) generateChildren() *QuadMap {
+	var originOffset = float64(MAX_DISTANCE / m.zoomLevel) / 2
 
-  m.subMaps[0] = zeroMap
-  m.subMaps[1] = oneMap
-  m.subMaps[2] = twoMap
-  m.subMaps[3] = threeMap
+	zeroMap := &QuadMap{m, m.zoomLevel + 1, [4]*QuadMap{}, nil, m.originX+originOffset, m.originY+originOffset}
+	oneMap := &QuadMap{m, m.zoomLevel + 1, [4]*QuadMap{}, nil, m.originX-originOffset, m.originY+originOffset}
+	twoMap := &QuadMap{m, m.zoomLevel + 1, [4]*QuadMap{}, nil, m.originX-originOffset, m.originY-originOffset}
+	threeMap := &QuadMap{m, m.zoomLevel + 1, [4]*QuadMap{}, nil, m.originX+originOffset, m.originY-originOffset}
 
-  for _, l := range m.loads {
-    if zeroMap.HasPoint(l.Pickup) {
-      zeroMap.AddLoad(l)
-    } else if oneMap.HasPoint(l.Pickup) {
-      oneMap.AddLoad(l)
-    } else if twoMap.HasPoint(l.Pickup) {
-      twoMap.AddLoad(l)
-    } else if threeMap.HasPoint(l.Pickup) {
-      threeMap.AddLoad(l)
-    }
-  }
-  return m
+	m.subMaps[0] = zeroMap
+	m.subMaps[1] = oneMap
+	m.subMaps[2] = twoMap
+	m.subMaps[3] = threeMap
+
+	for _, l := range m.loads {
+		if zeroMap.HasPoint(l.Pickup) {
+			zeroMap.AddLoad(l)
+		} else if oneMap.HasPoint(l.Pickup) {
+			oneMap.AddLoad(l)
+		} else if twoMap.HasPoint(l.Pickup) {
+			twoMap.AddLoad(l)
+		} else if threeMap.HasPoint(l.Pickup) {
+			threeMap.AddLoad(l)
+		}
+	}
+	return m
 }
 
-func (m* QuadMap) GetChildren() [4]*QuadMap {
-  if m.subMaps[0] == nil {
-    m.generateChildren()
-    return m.subMaps
-  }
+func (m *QuadMap) GetChildren() [4]*QuadMap {
+	if m.subMaps[0] == nil {
+		m.generateChildren()
+		return m.subMaps
+	}
 
-  return m.subMaps
+	return m.subMaps
 }
 
-func NewQuadMap(loads []*Load, depth int) *QuadMap {
-  minX, maxX, minY, maxY := findBounds(loads)
-  quadMap := QuadMap{nil, [4]*QuadMap{}, loads, minX, maxX, minY, maxY}
-  quadMap.generateChildren()
+func NewQuadMap(loads []*Load) *QuadMap {
+	quadMap := QuadMap{nil, 1, [4]*QuadMap{}, loads, 0, 0}
 
-  return &quadMap
-}
-
-// Finds bounds of loads using pickup and dropoff locations
-func findBounds(loads []*Load) (float64, float64, float64, float64) {
-  var minX, maxX, minY, maxY float64
-  for _, l := range loads {
-    if l.Pickup.X < minX {
-      minX = l.Pickup.X
-    }
-    if l.Pickup.X > maxX {
-      maxX = l.Pickup.X
-    }
-    if l.Pickup.Y < minY {
-      minY = l.Pickup.Y
-    }
-    if l.Pickup.Y > maxY {
-      maxY = l.Pickup.Y
-    }
-
-    if l.Dropoff.X < minX {
-      minX = l.Dropoff.X
-    }
-    if l.Dropoff.X > maxX {
-      maxX = l.Dropoff.X
-    }
-    if l.Dropoff.Y < minY {
-      minY = l.Dropoff.Y
-    }
-    if l.Dropoff.Y > maxY {
-      maxY = l.Dropoff.Y
-    }
-  }
-  return minX, maxX, minY, maxY
+	return &quadMap
 }
--- entities/quad_map_test.go
+++ entities/quad_map_test.go
@@ -1,60 +1,124 @@
 package entities
 
 import (
-  "testing"
+	"testing"
 )
 
-func TestFindBounds(t *testing.T) {
-  loadOne := NewLoad(1, Point{1, 1}, Point{2, 2})
-  loadTwo := NewLoad(2, Point{-2, -2}, Point{3, 2})
+func TestGenerateChildrenShouldAddLoadsToCorrectQuad(t *testing.T) {
+	loadOne := NewLoad(1, Point{1, 1}, Point{-2, 2})
+	outOfBoundsLoad := NewLoad(2, Point{360.01, 2}, Point{3, 2})
+	loadTwo := NewLoad(2, Point{-2, 2}, Point{3, 2})
+	loadThree := NewLoad(3, Point{-4, -3}, Point{-5, -2})
+	loadFour := NewLoad(4, Point{7, -3}, Point{5, -2})
 
-  loads := []*Load{&loadOne, &loadTwo}
-  
-  minX, maxX, minY, maxY := findBounds(loads)
+	loads := []*Load{&loadOne, &loadTwo, &loadThree, &loadFour, &outOfBoundsLoad}
 
-  if minX != -2 {
-    t.Error("Expected minX to be -2, got ", minX)
-  }
+	quadMap := NewQuadMap(loads)
 
-  if maxX != 3 {
-    t.Error("Expected maxX to be 3, got ", maxX)
-  }
+	quadMap.generateChildren()
 
-  if minY != -2 {
-    t.Error("Expected minY to be -2, got ", minY)
-  }
+	if len(quadMap.subMaps[0].loads) != 1 {
+		t.Error("Expected subMap[0] to have 1 load, got ", len(quadMap.subMaps[0].loads))
+	}
 
-  if maxY != 2 {
-    t.Error("Expected maxY to be 2, got ", maxY)
-  }
+	if len(quadMap.subMaps[1].loads) != 1 {
+		t.Error("Expected subMap[1] to have 1 load, got ", len(quadMap.subMaps[1].loads))
+	}
+
+	if len(quadMap.subMaps[2].loads) != 1 {
+		t.Error("Expected subMap[2] to have 1 load, got ", len(quadMap.subMaps[2].loads))
+	}
+
+	if len(quadMap.subMaps[3].loads) != 1 {
+		t.Error("Expected subMap[3] to have 1 load, got ", len(quadMap.subMaps[3].loads))
+	}
 }
 
-func TestGenerateChildrenShouldAddLoadsToCorrectQuad(t *testing.T) {
-  loadOne := NewLoad(1, Point{-1, 1}, Point{-2, 2})
-  loadTwo := NewLoad(2, Point{2, 2}, Point{3, 2})
-  loadThree := NewLoad(3, Point{-4, -3}, Point{-5, -2})
-  loadFour := NewLoad(4, Point{7, -3}, Point{5, -2})
+func TestOriginPlacement(t *testing.T) {
+	quadMap := NewQuadMap([]*Load{})
+	
+	if quadMap.originX != 0 {
+		t.Error("Expected originX to be 0, got ", quadMap.originX)
+	}
 
-  loads := []*Load{&loadOne, &loadTwo, &loadThree, &loadFour}
-  
-  minX, maxX, minY, maxY := findBounds(loads)
-  quadMap := QuadMap{loads: loads, minX: minX, maxX: maxX, minY: minY, maxY: maxY}
+	if quadMap.originY != 0 {
+		t.Error("Expected originY to be 0, got ", quadMap.originY)
+	}
 
-  quadMap.generateChildren()
+	quadZero := quadMap.GetChildren()[0]
 
-  if len(quadMap.subMaps[0].loads) != 1 {
-    t.Error("Expected subMap[0] to have 1 load, got ", len(quadMap.subMaps[0].loads))
-  }
+	if quadZero.originX != 180 {
+		t.Error("Expected quadZero originX to be 180, got ", quadZero.originX)
+	}
 
-  if len(quadMap.subMaps[1].loads) != 1 {
-    t.Error("Expected subMap[1] to have 1 load, got ", len(quadMap.subMaps[1].loads))
-  }
+	if quadZero.originY != 180 {
+		t.Error("Expected quadZero originY to be 180, got ", quadZero.originY)
+	}
 
-  if len(quadMap.subMaps[2].loads) != 1 {
-    t.Error("Expected subMap[2] to have 1 load, got ", len(quadMap.subMaps[2].loads))
-  }
+	quadZeroZero := quadZero.GetChildren()[0]
+	if quadZeroZero.originX != 270 {
+		t.Error("Expected quadZeroZero originX to be 270, got ", quadZeroZero.originX)
+	}
 
-  if len(quadMap.subMaps[3].loads) != 1 {
-    t.Error("Expected subMap[3] to have 1 load, got ", len(quadMap.subMaps[3].loads))
-  }
+	if quadZeroZero.originY != 270 {
+		t.Error("Expected quadZeroZero originY to be 270, got ", quadZeroZero.originY)
+	}
+
+	quadZeroOne := quadZero.GetChildren()[1]
+	if quadZeroOne.originX != 90 {
+		t.Error("Expected quadZeroOne originX to be 90, got ", quadZeroOne.originX)
+	}
+
+	if quadZeroOne.originY != 270 {
+		t.Error("Expected quadZeroOne originY to be 270, got ", quadZeroOne.originY)
+	}
+
+	quadZeroTwo := quadZero.GetChildren()[2]
+	if quadZeroTwo.originX != 90 {
+		t.Error("Expected quadZeroTwo originX to be 90, got ", quadZeroTwo.originX)
+	}
+
+	if quadZeroTwo.originY != 90 {
+		t.Error("Expected quadZeroTwo originY to be 90, got ", quadZeroTwo.originY)
+	}
+	
+	quadZeroThree := quadZero.GetChildren()[3]
+	if quadZeroThree.originX != 270 {
+		t.Error("Expected quadZeroThree originX to be 270, got ", quadZeroThree.originX)
+	}
+
+	if quadZeroThree.originY != 90 {
+		t.Error("Expected quadZeroThree originY to be 90, got ", quadZeroThree.originY)
+	}
+}
+
+func TestZoomLevelsWork(t *testing.T) {
+	loadOne := NewLoad(1, Point{0, 0}, Point{-2, 2})
+	loadTwo := NewLoad(2, Point{180, 120}, Point{3, 2})
+	loadThree := NewLoad(3, Point{181, 181}, Point{3, 2})
+	loadFour := NewLoad(4, Point{-1, 2}, Point{3, 2})
+
+	loads := []*Load{&loadOne, &loadTwo, &loadThree, &loadFour}
+
+	quadMap := NewQuadMap(loads)
+	if len(quadMap.loads) != 4 {
+		t.Error("Expected quadMap to have 4 load, got ", len(quadMap.loads))
+	}
+
+	quadZero := quadMap.GetChildren()[0]
+	if len(quadZero.loads) != 3 {
+		t.Error("Expected quadZeroZero to have 3 load, got ", len(quadZero.loads))
+	}
+
+	quadZeroTwo := quadZero.GetChildren()[2]
+
+	if len(quadZeroTwo.loads) != 2 {
+		t.Error("Expected quadZeroTwo to have 2 load, got ", len(quadZeroTwo.loads))
+	}
+
+	quadZeroZero := quadZeroTwo.GetChildren()[0]
+
+	if len(quadZeroZero.loads) != 1 {
+		t.Error("Expected quadZeroZero to have 1 load, got ", len(quadZeroZero.loads))
+	}
 }
