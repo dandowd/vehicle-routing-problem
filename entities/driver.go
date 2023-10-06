e71b9e5b7424038618a125db1be17854f292d17b
Dan Dowd <danieldowd89@gmail.com>
2023-10-05 21:00:24 -0600

    init

---
+++ entities/driver.go
@@ -0,0 +1,11 @@
+package main
+
+type Driver struct {
+	id             int
+	currentPoint   Point
+	completedLoads []Load
+}
+
+func (d* Driver) CompleteLoad(l Load) {
+	d.completedLoads = append(d.completedLoads, l)
+}
---
+++ entities/load.go
@@ -0,0 +1,17 @@
+package main
+
+import "math"
+
+type Load struct {
+	LoadNumber int
+	Pickup     Point
+	Dropoff    Point
+}
+
+func (l* Load) GetTime() float64 {
+	return math.Sqrt(math.Pow(l.Pickup.X - l.Dropoff.X, 2) + math.Pow(l.Pickup.Y - l.Dropoff.Y, 2))
+}
+
+func NewLoad(loadNumber int, pickup Point, dropoff Point) Load {
+	return Load{loadNumber, pickup, dropoff}
+}
