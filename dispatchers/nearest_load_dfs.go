4edb03420aea1b8b004eeb2609a90de5c47825ff
Dan Dowd <danieldowd89@gmail.com>
2023-10-07 20:17:46 -0600

    adds first attempt at dfs

---
+++ dispatchers/nearest_load_dfs.go
@@ -0,0 +1,86 @@
+package dispatchers
+
+import (
+	"math"
+	"vehicle-routing-problem/entities"
+)
+
+type NearestLoadDFSDispatch struct {
+	loads            map[int]*entities.Load
+	numberOfAdjacent int
+}
+
+func NewNearestLoadDFSDispatch(loads []*entities.Load, numberOfAdjacent int) *NearestLoadDFSDispatch {
+	dispatch := &NearestLoadDFSDispatch{loads: make(map[int]*entities.Load)}
+	for _, load := range loads {
+		dispatch.loads[load.LoadNumber] = load
+	}
+
+	return dispatch
+}
+
+// Recursively check paths until the path with the least waste is found
+// Base case: once a driver reaches a node with all adjacent nodes being unable to be moved
+func (d *NearestLoadDFSDispatch) search(currentPoint entities.Point, travelCosts float64, totalCost float64, path map[int]*entities.Load) (float64, map[int]*entities.Load) {
+  nearestLoads := d.getNearestLoads(currentPoint, path)
+  bestPath := path
+  bestCosts := math.MaxFloat64
+
+  for _, loadMap := range nearestLoads {
+    pickupCosts := currentPoint.DistanceTo(loadMap.Pickup)
+    returnCosts := loadMap.Dropoff.DistanceTo(entities.Point{X: 0, Y: 0})
+
+    tripCosts := pickupCosts + returnCosts + totalCost + loadMap.GetTime()
+
+    if tripCosts > entities.MAX_DRIVE_TIME {
+      continue
+    }
+
+    currentPath := copyPath(path)
+
+    costs, subPath := d.search(loadMap.Dropoff, travelCosts, tripCosts + totalCost, currentPath)
+
+    if costs < bestCosts {
+      bestPath = subPath
+    }
+  }
+
+  if bestCosts == math.MaxFloat64 {
+    return travelCosts, path
+  }
+
+  return travelCosts + bestCosts, bestPath
+}
+
+func (d *NearestLoadDFSDispatch) getNearestLoads(point entities.Point, path map[int]*entities.Load) []*entities.Load {
+	var nearestLoads []*entities.Load
+
+	for _, load := range d.loads {
+		if path[load.LoadNumber] != nil {
+			continue
+		}
+
+		dist := point.DistanceTo(load.Pickup)
+		if len(nearestLoads) <= d.numberOfAdjacent {
+			nearestLoads = append(nearestLoads, load)
+			continue
+		}
+
+		for i, currentNearest := range nearestLoads {
+      currentDist := point.DistanceTo(currentNearest.Pickup)
+			if dist < currentDist {
+				nearestLoads[i] = load
+      }
+		}
+	}
+
+	return nearestLoads
+}
+
+func copyPath(path map[int]*entities.Load) map[int]*entities.Load {
+  newPath := make(map[int]*entities.Load)
+  for k, v := range path {
+    newPath[k] = v
+  }
+  return newPath
+}
---
+++ dispatchers/nearest_load_dfs_test.go
@@ -0,0 +1,22 @@
+package dispatchers
+
+import (
+	"fmt"
+	"testing"
+	"vehicle-routing-problem/entities"
+)
+
+func TestSearchShouldReturnBestPath(t *testing.T) {
+  loads := []*entities.Load{
+    entities.NewLoad(1, entities.Point{X: 1, Y: 1}, entities.Point{X: 2, Y: 2}),
+    entities.NewLoad(2, entities.Point{X: 2, Y: 2}, entities.Point{X: 3, Y: 3}),
+    entities.NewLoad(3, entities.Point{X: 3, Y: 3}, entities.Point{X: 4, Y: 4}),
+    entities.NewLoad(4, entities.Point{X: 4, Y: 4}, entities.Point{X: 5, Y: 5}),
+  }
+
+  dispatch := NewNearestLoadDFSDispatch(loads, 4)
+
+  cost, paths := dispatch.search(entities.Point{X: 0, Y: 0}, 0, 0, make(map[int]*entities.Load))
+
+  fmt.Println(cost, paths)
+}
