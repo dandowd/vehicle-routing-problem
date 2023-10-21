67ff142aa3d19b3e0c3cd50cda1363b0ac62a8db
Dan Dowd <danieldowd89@gmail.com>
2023-10-21 14:49:08 -0600

    adds dropoff and pickup glyphs
    formats

--- visualization/route.go
+++ visualization/route.go
@@ -8,69 +8,100 @@ import (
 	"gonum.org/v1/plot/plotter"
 	"gonum.org/v1/plot/plotutil"
 	"gonum.org/v1/plot/vg"
+	"gonum.org/v1/plot/vg/draw"
 )
 
 func Route(drivers []*entities.Driver, title string) {
-  p := plot.New()
-  p.Title.Text = title
+	p := plot.New()
+	p.Title.Text = title
 
-  points := plotter.XYs{}
-  
-  for driverIndex, driver := range drivers {
-    lastPoint := plotter.XY{X: 0, Y: 0}
+	pickupPoints := plotter.XYs{}
+	dropoffPoints := plotter.XYs{}
 
-    for _, load := range driver.GetPath() {
-      pickup := plotter.XY{X: load.Pickup.X, Y: load.Pickup.Y}
-      dropoff := plotter.XY{X: load.Dropoff.X, Y: load.Dropoff.Y}
-    
-      lineLine := getLoadLine(pickup, dropoff, driverIndex)
-      p.Add(lineLine)
+	for driverIndex, driver := range drivers {
+		lastPoint := plotter.XY{X: 0, Y: 0}
 
-      travelLine := getTravelLine(lastPoint, pickup, driverIndex)
-      p.Add(travelLine)
+		for _, load := range driver.GetPath() {
+			pickup := plotter.XY{X: load.Pickup.X, Y: load.Pickup.Y}
+			pickupPoints = append(pickupPoints, pickup)
 
-      lastPoint = dropoff
-    }
-     
-    line := getTravelLine(lastPoint, plotter.XY{X: 0, Y: 0}, driverIndex)
-    p.Add(line)
+			dropoff := plotter.XY{X: load.Dropoff.X, Y: load.Dropoff.Y}
+			dropoffPoints = append(dropoffPoints, dropoff)
 
-    p.Legend.Add(fmt.Sprint(driverIndex), line)
-  }
-  
-  scatter, err := plotter.NewScatter(points)
-  if err != nil {
-    panic(err)
-  }
+			travelLine := newTravelLine(lastPoint, pickup, driverIndex)
+			p.Add(travelLine)
 
-  scatter.GlyphStyle.Radius = vg.Points(10)
-  p.Add(scatter)
+			lineLine := newLoadLine(pickup, dropoff, driverIndex)
+			p.Add(lineLine)
 
-  if err := p.Save(20*vg.Inch, 20*vg.Inch, fmt.Sprint("route.png")); err != nil {
+			lastPoint = dropoff
+		}
+
+		line := newTravelLine(lastPoint, plotter.XY{X: 0, Y: 0}, driverIndex)
+		p.Add(line)
+
+		p.Legend.Add(fmt.Sprint(driverIndex), line)
+	}
+
+	createPickupScatter(pickupPoints, p)
+
+	createDropoffScatter(dropoffPoints, p)
+
+	if err := p.Save(20*vg.Inch, 20*vg.Inch, fmt.Sprint("route.png")); err != nil {
 		panic("Could not save the plot to a PNG file")
 	}
 }
 
-func getLoadLine(pickup plotter.XY, dropoff plotter.XY, driverIndex int) *plotter.Line {
-    line, err := plotter.NewLine(plotter.XYs{pickup, dropoff})
-    if err != nil {
-      panic(err)
-    }
-    
-    line.Color = plotutil.Color(driverIndex)
+func createPickupScatter(pickupPoints plotter.XYs, p *plot.Plot) *plotter.Scatter {
+	scatter, err := plotter.NewScatter(pickupPoints)
+	if err != nil {
+		panic(err)
+	}
 
-    return line
+	scatter.GlyphStyle.Shape = draw.BoxGlyph{}
+	scatter.GlyphStyle.Radius = vg.Points(5)
+
+	p.Add(scatter)
+	p.Legend.Add("Pickup", scatter)
+
+	return scatter
 }
 
-func getTravelLine(lastPoint plotter.XY, pickup plotter.XY, driverIndex int) *plotter.Line {
-    travelLine, err := plotter.NewLine(plotter.XYs{lastPoint, pickup})
-    travelLine.LineStyle.Dashes = []vg.Length{vg.Points(5), vg.Points(5)}
-    if err != nil {
-      panic(err)
-    }
+func createDropoffScatter(dropoffPoints plotter.XYs, p *plot.Plot) *plotter.Scatter {
+	scatter, err := plotter.NewScatter(dropoffPoints)
+	if err != nil {
+		panic(err)
+	}
 
-    travelLine.Color = plotutil.Color(driverIndex)
-    travelLine.LineStyle.Dashes = []vg.Length{vg.Points(5), vg.Points(5)}
+	scatter.GlyphStyle.Shape = draw.SquareGlyph{}
+	scatter.GlyphStyle.Radius = vg.Points(5)
 
-    return travelLine
+	p.Add(scatter)
+	p.Legend.Add("Dropoff", scatter)
+
+	return scatter
+}
+
+func newLoadLine(pickup plotter.XY, dropoff plotter.XY, driverIndex int) *plotter.Line {
+	line, err := plotter.NewLine(plotter.XYs{pickup, dropoff})
+	if err != nil {
+		panic(err)
+	}
+
+	line.Color = plotutil.Color(driverIndex)
+
+	return line
+}
+
+func newTravelLine(lastPoint plotter.XY, pickup plotter.XY, driverIndex int) *plotter.Line {
+	travelLine, err := plotter.NewLine(plotter.XYs{lastPoint, pickup})
+	travelLine.LineStyle.Dashes = []vg.Length{vg.Points(5), vg.Points(5)}
+	if err != nil {
+		panic(err)
+	}
+
+	travelLine.Color = plotutil.Color(driverIndex)
+	travelLine.LineStyle.Dashes = []vg.Length{vg.Points(5), vg.Points(5)}
+
+	return travelLine
 }
