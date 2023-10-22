24199b3c630176e87c20e9d78901b4d9f01d2824
Dan Dowd <danieldowd89@gmail.com>
2023-10-21 21:00:21 -0600

    adds cli library

--- main.go
+++ main.go
@@ -3,30 +3,48 @@ package main
 import (
 	"fmt"
 	"os"
-	"vehicle-routing-problem/utils"
 	"vehicle-routing-problem/dispatchers"
+	"vehicle-routing-problem/utils"
 	"vehicle-routing-problem/visualization"
+
+	"github.com/urfave/cli/v2"
 )
 
 func main() {
-	if len(os.Args) < 2 {
-		fmt.Println("Usage: go run main.go <load-file> [visualization-name]")
-		os.Exit(1)
+	app := &cli.App{
+		Flags: []cli.Flag{
+			&cli.StringFlag{
+				Name:    "driver-route-file",
+				Aliases: []string{"r"},
+				Usage:   "Drivers routes file path",
+			},
+			&cli.StringFlag{
+				Name:    "annealing-graph",
+				Aliases: []string{"ag"},
+				Usage:   "Annealing graph file path",
+			},
+		},
+		Action: func(c *cli.Context) error {
+			filepath := c.Args().First()
+			loads := utils.ParseLoadFile(filepath)
+
+			nearestLoadDrivers := dispatchers.NewNearestLoadDispatch(loads).SearchForRoutes()
+			fmt.Println("Nearest Load Total Cost:", dispatchers.GetTotalCost(nearestLoadDrivers))
+
+			drivers := dispatchers.Annealing(loads)
+			fmt.Println("Annealing Total Cost:", dispatchers.GetTotalCost(drivers))
+
+			routeFile := c.String("route")
+			if routeFile != "" {
+				title := fmt.Sprintf("Total Cost: %f", dispatchers.GetTotalCost(drivers))
+				visualization.Route(drivers, title, routeFile)
+			}
+
+			return nil
+		},
 	}
 
-	filepath := os.Args[1]
-	loads := utils.ParseLoadFile(filepath)
-
-	nearestLoadDrivers := dispatchers.NewNearestLoadDispatch(loads).SearchForRoutes()
-	fmt.Println("Nearest Load Total Cost:", dispatchers.GetTotalCost(nearestLoadDrivers))
-
-	drivers := dispatchers.Annealing(loads)
-	fmt.Println("Annealing Total Cost:", dispatchers.GetTotalCost(drivers))
-
-	if len(os.Args) == 3 {
-		visualizationName := os.Args[2]
-
-		title := fmt.Sprintf("Total Cost: %f", dispatchers.GetTotalCost(drivers))
-		visualization.Route(drivers, title, fmt.Sprint("annealing-", visualizationName))
+	if err := app.Run(os.Args); err != nil {
+		panic(err)
 	}
 }
